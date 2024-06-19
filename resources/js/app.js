158c5da2e661f038cbf5f196d7470b3700844fa6
Pedro Hayashi <pedrohayashi1496@gmail.com>
2024-06-19 15:56:56 -0300

    Ajustes css e inclusão de categorias/menu mobile

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,44 @@
-import './bootstrap';
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 50,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
+  });
+
+  const swiper2 = new Swiper('.swiper-event', {
+    // Default parameters
+    slidesPerView: 1,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 5,
+      }
+    }
+  })
+
